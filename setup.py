b919748632a60b6f516f70defc3638e01e4ec89b
R-Sai-Chirag <saichirag355@gmail.com>
2025-08-24 11:23:11 +0530

    environment setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="medi_GPT",
+    version="0.1.0",
+    author="Sai Chirag",
+    author_email="saichirag355@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
